85aafcdc79a1678696ebf7d6f695a2cb18d8af48
Aman Tiwari <amantiwat776@gmail.com>
2024-04-29 14:01:27 +0530

    fixed typo

---
+++ src/app/route/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <input
+        type="text"
+        className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+      />
+    </div>
+  );
+}
+
+export default page;
